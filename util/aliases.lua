5a33063209e36c1d220809ac60a65f0306568c1b
DrakeFS <drake.fsx@gmail.com>
2024-06-14 01:29:11 -0400

    Added #FO and #ANV keys

--- util/aliases.lua
+++ util/aliases.lua
@@ -5,6 +5,8 @@ local Aliases = {}
 --------------------------------------------------------------------------------
 function Aliases.Update()
     windower.send_command('alias usekeys input //keys use "SP Gobbie Key"')
+    windower.send_command('alias usekeyfo input //keys use "Dial Key #Fo"')
+    windower.send_command('alias usekeyanv input //keys use "Dial Key #ANV"')
 end
 
 return Aliases
